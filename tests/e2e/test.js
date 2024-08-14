c2f5e693481ceead20a46f8cbb7cbbc73897cd4b
Kaung Zin Hein <kaungzinhein113@gmail.com>
2024-08-14 13:55:21 -0400

    update: separate jsdoc

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -4,8 +4,7 @@ const { findLib } = require("../../lib/index");
 
 describe("findLib", () => {
   describe("given that a valid search is made", () => {
-    it("should display the correct tree structure", async () => {
-
+    it("should display the correct tree structure if the `size` as find options", async () => {
       const testFn = async () => {
         await findLib("cross-spawn", { size: 1 });
       };
@@ -51,7 +50,55 @@ describe("findLib", () => {
       };
       const expectedTree = JSON.stringify(tree, null, 2);
 
-      expect(testFn).toLogStdout(expectedTree);
+      await expect(testFn).toLogStderr(expectedTree);
+    });
+    it.only("should display the correct tree structure without any find options", async () => {
+      const testFn = async () => {
+        await findLib("cross-spawn");
+      };
+      const tree = {
+        root: {
+          deps: {
+            "path-key": {
+              deps: {},
+              version: "3.1.0",
+              name: "path-key",
+            },
+            "shebang-command": {
+              deps: {
+                "shebang-regex": {
+                  deps: {},
+                  version: "3.0.0",
+                  name: "shebang-regex",
+                },
+              },
+              version: "2.0.0",
+              name: "shebang-command",
+            },
+            which: {
+              deps: {
+                isexe: {
+                  deps: {},
+                  version: "2.0.0",
+                  name: "isexe",
+                },
+              },
+              version: "2.0.1",
+              name: "which",
+            },
+          },
+          version: "7.0.3",
+          name: "cross-spawn",
+        },
+        ttlDeps: 5,
+        layers: 2,
+        firstLayer: 3,
+        _opts: {},
+        earlyReturn: false,
+      };
+      const expectedTree = JSON.stringify(tree, null, 2);
+
+      await expect(testFn).toLogStderr(expectedTree);
     });
   });
 });
